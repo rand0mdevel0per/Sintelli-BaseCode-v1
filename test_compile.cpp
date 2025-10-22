7f4263b0f32e432dc7c5792c19467dbd7b267113
rand0mdevel0per <rand0mk4cas@gmail.com>
2025-10-23 01:32:23 +0800

    Did some clearing...

--- test_compile.cpp
+++
@@ -1,7 +0,0 @@
-// 简单编译测试文件
-#include "Neuron.cu"
-#include "KFEManager.h"
-
-int main() {
-    return 0;
-}
--- test_cpp.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "C++ test successful" << std::endl;
-    return 0;
-}
